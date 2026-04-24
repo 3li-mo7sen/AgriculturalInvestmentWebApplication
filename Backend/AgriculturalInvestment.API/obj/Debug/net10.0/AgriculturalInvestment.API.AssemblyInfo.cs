d336443404e6b6a45c95e96c812ddb8169a4f573
Ali Mohsen <3li.mo7sen.m7moud@gmail.com>
2026-04-24 05:54:03 +0300

    add frontend, documentation, AI folders

--- Backend/AgriculturalInvestment.API/obj/Debug/net10.0/AgriculturalInvestment.API.AssemblyInfo.cs
+++ Backend/AgriculturalInvestment.API/obj/Debug/net10.0/AgriculturalInvestment.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f6e7bcd65a830d65555e2699623826fca32128b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
