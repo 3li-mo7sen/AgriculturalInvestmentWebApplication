1c33ede3a6a07c8742246015c0c4d2d9e2c87051
Fadell <abwalfdlrmdan128@gmail.com>
2026-04-27 01:33:00 +0300

    deleted weather controller

--- AgriculturalInvestment.API/obj/Debug/net10.0/AgriculturalInvestment.API.AssemblyInfo.cs
+++ AgriculturalInvestment.API/obj/Debug/net10.0/AgriculturalInvestment.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02007dd4356b22bb82ffcc158bc089fe52dfbac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1732580ea427c9ad3fb8154adb2d87c40b3c32")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgriculturalInvestment.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
