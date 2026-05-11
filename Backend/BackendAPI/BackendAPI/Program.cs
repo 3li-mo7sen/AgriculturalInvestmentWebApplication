45bb295193466339647613c67a853af8550cdab9
Mayar-mohamed1 <mayarabdelbaset2016@gmail.com>
2026-05-11 08:18:33 +0300

    home structure&Font awesome

--- Backend/BackendAPI/BackendAPI/Program.cs
+++ Backend/BackendAPI/BackendAPI/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:50547")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
--- Frontend/AgriPro/src/app/components/home/home.ts
+++ Frontend/AgriPro/src/app/components/home/home.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { HowItWorks } from './components/how-it-works/how-it-works';
+import { Testimonals } from './components/testimonals/testimonals';
+import { Features } from './components/features/features';
+import { ProjectList } from './components/project-list/project-list';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar,Hero,HowItWorks,Features,ProjectList,Testimonals,Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
