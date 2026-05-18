db62e247d4a23524b487e630ff7a65c5c26dcb7f
Mayar_mohamed1 <mayarabdelbaset2016@gmail.com>
2026-05-18 10:50:03 +0300

    bb

--- Frontend/AgriPro/src/app/components/farmer/components/create-project/components/create-project-forms/create-project-forms.ts
+++ Frontend/AgriPro/src/app/components/farmer/components/create-project/components/create-project-forms/create-project-forms.ts
@@ -5,6 +5,8 @@ import { LandImages } from '../land-images/land-images';
 import { FarmerDocuments } from '../farmer-documents/farmer-documents';
 import { FarmerInvestment } from '../farmer-investment/farmer-investment';
 import { CommonModule } from '@angular/common';
+import { FarmerService } from '../../../../../../services/farmerService/farmer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-project-forms',
@@ -13,7 +15,25 @@ import { CommonModule } from '@angular/common';
   styleUrl: './create-project-forms.css',
 })
 export class CreateProjectForms {
-  currentStep = 0; // الخطوة الحالية تبدأ من 0
+  currentStep = 0;
+
+  finalProjectData: any = {
+    title: "",
+    shortDescription: "",
+    fullDescription: "",
+    cropType: "",
+    governorate: "",
+    landSize: "",
+    soilType: "",
+    waterSource: "",
+    cost: 0,
+    targetAmount: 0,
+    minInvestment: 0,
+    expectedProfit: 0,
+    expectedRoi: "",
+    duration: 0,
+    farmerShare: 0
+  };
 
   steps = [
     { label: 'Basic Info', icon: 'fas fa-info-circle' },
@@ -23,6 +43,13 @@ export class CreateProjectForms {
     { label: 'Investment', icon: 'fas fa-coins' }
   ];
 
+  constructor(private _FarmerService:FarmerService,private router:Router) { }
+
+  handleStepData(data: any) {
+    this.finalProjectData = { ...this.finalProjectData, ...data };
+    this.nextStep();
+  }
+  
   nextStep() {
     if (this.currentStep < this.steps.length - 1) {
       this.currentStep++;
@@ -33,6 +60,16 @@ export class CreateProjectForms {
     if (this.currentStep > 0) {
       this.currentStep--;
     }
+  }
+
+  submitToApi() {
+    this._FarmerService.createProject(this.finalProjectData).subscribe({
+      next: (res) => {
+        alert('project submitted successfully!');
+        this.router.navigate(['/farmer/my-projects']);
+      },
+      error: (err) => alert('project submit failed!')
+    });
   }
 
 }
--- Frontend/AgriPro/src/app/components/farmer/components/create-project/components/land-form/land-form.ts
+++ Frontend/AgriPro/src/app/components/farmer/components/create-project/components/land-form/land-form.ts
@@ -8,6 +8,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./land-form.css'],
 })
 export class LandForm {
-  @Output() continue = new EventEmitter<void>();
+  @Output() continue = new EventEmitter<any>();
   @Output() previous = new EventEmitter<void>();
+
+  data = {
+    governorate: '',
+    landSize: '',
+    soilType: '',
+    waterSource: '',
+    ownershipType: ''
+  };
+
+  sendData() {
+    this.continue.emit(this.data);
+  }
 }
